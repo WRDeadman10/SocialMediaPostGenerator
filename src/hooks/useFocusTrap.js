9d0a39ed65ea1c860ace5f15bb882fd2e3ad1634
vinayak-vc <vinayak.lakhani@viitor.cloud>
2026-05-01 16:45:34 +0530

    bridge: task 1101 modify src/hooks/useFocusTrap.js

--- src/hooks/useFocusTrap.js
+++ src/hooks/useFocusTrap.js
@@ -2,3 +2,7 @@ export const useFocusTrap = () => {
   throw new Error("bridge placeholder - will be overwritten by executor");
 };
 
+export const useFocusTrap = () => {
+  throw new Error("bridge placeholder - will be overwritten by executor");
+};
+
